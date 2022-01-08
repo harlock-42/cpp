2286f0fac4853c06022f4a938eada593a8c85ddc
Harlock <allaire.terry.at>
2022-01-08 22:37:48 +0100

    save

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -2,6 +2,7 @@
 # define ARRAY_H
 
 # include <iostream>
+# include <exception>
 
 template< typename T >
 class Array
@@ -20,13 +21,24 @@ class Array
 
 		Array( unsigned int i = 0 ) : _array( new T[i] ), _size(i)
 		{
+			for (size_t i = 0; i < this->getSize(); ++i)
+			{
+				this->_array[i] = T();
+			}
 			return;
+		}
+
+		// Constructor by copie
+
+		Array( const Array<T> &rhs ) : _array( new T[rhs.getSize()] ), _size( rhs.getSize())
+		{
+			for (size_t i = 0; i < rhs.getSize(); ++i)
+				this->_array[i] = rhs[i];
 		}
 
 		~Array( void )
 		{
-			delete [] this->getArray();
-			std::cout << "destruction\n";
+			delete [] this->_array;
 		}
 
 		// Getter
@@ -36,13 +48,55 @@ class Array
 			return (this->_array);
 		}
 
+		unsigned int	getSize( void ) const
+		{
+			return (this->_size);
+		}
+
 		// Overload
 
-		T	&operator[]( int index )
+		Array<T>	&operator=(const Array<T> &rhs )
 		{
-			std::cout << index << std::endl;
-			return (_array[index]);
+			this->_size = rhs.getSize();
+			delete [] this->getArray();
+			this->_array = new T[rhs.getSize()];
+			for (size_t i = 0; i < rhs.getSize(); ++i)
+				this->_array[i] = rhs[i];
+			return (*this);
 		}
+
+		T	&operator[]( unsigned int index ) const
+		{
+			if (index >= this->getSize())
+				throw OutOfLimitException();
+			return (this->_array[index]);
+		}
+
+		// exception
+
+		class OutOfLimitException : public std::exception
+		{
+			public:
+
+				virtual const char *what( void ) const throw()
+				{
+					return ("Index out of range");
+				}
+		};
 };
+
+template < typename T >
+std::ostream	&operator<<( std::ostream &os, const Array<T> &array )
+{
+	os << "[";
+	for ( size_t i = 0; i < array.getSize(); ++i)
+	{
+		os << array[i];
+		if (i < array.getSize() - 1)
+			os << ", ";
+	}
+	os << "]";
+	return (os);
+}
 
 #endif
--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -2,10 +2,25 @@
 
 int	main( void )
 {
-	Array <int>intArray(2);
+	Array <int>intArray(10);
+
+	for (size_t	i = 0; i < 10; ++i)
+		intArray[i] = i;
+	std::cout << intArray << std::endl;
+	Array<int>	arr2 = intArray;
+
+	arr2 = arr2;
 
 
-	intArray[3];
-	intArray[0] = 3;
-	// std::cout << intArray[0] << std::endl;
+
+	try
+	{
+		arr2[100] = 100;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	std::cout << arr2 << std::endl;
 }
